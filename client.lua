df1e3e37c7d3a28d6d021180a91a866af6fc18c8
RaulGG <66495931+RauhGG@users.noreply.github.com>
2022-06-18 22:05:05 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,19 @@
+print("[RUI] Client loaded")
+
+CreateThread(function()
+    while true do
+        TriggerEvent('esx_status:getStatus', 'hunger', function(status)
+            hunger = status.val / 10000
+        end)
+        TriggerEvent('esx_status:getStatus', 'thirst', function(status)
+            thirst = status.val / 10000
+        end)
+        SendNUIMessage({
+            food = hunger,
+            water = thirst,
+            o2 = GetPlayerUnderwaterTimeRemaining(PlayerId()) * 10,
+            health = (GetEntityHealth(PlayerPedId()) -100)
+        })
+        Wait(800)
+    end
+end)
